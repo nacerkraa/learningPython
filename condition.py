eb4ccc575fe262f746fd5b71952324e26e2460bf
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-03-05 03:12:43 +0000

    add condition files

---
+++ condition.py
@@ -0,0 +1,15 @@
+
+
+def main():
+    x, y = 100, 100
+    # condition folow user if, elif, else
+    if (x < y):
+        st = "x is less then y"
+    elif(x==y):
+        st = "x is equal then y"
+    else:
+        st = "x is greator then y"
+    print(st)
+
+if __name__ == "__main__":
+    main()
