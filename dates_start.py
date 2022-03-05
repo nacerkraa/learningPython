b938f9526f895ae784b8048f2d8f6e41e9b1248e
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-03-05 16:10:47 +0000

    add the datetimes file

---
+++ dates_start.py
@@ -0,0 +1,30 @@
+#
+# Example file for working with date information
+#
+
+
+def main():
+  ## DATE OBJECTS
+  # Get today's date from the simple today() method from the date class
+  from datetime import date
+  from datetime import time
+  from datetime import datetime
+
+  # print out the date's individual components
+  
+  
+  # retrieve today's weekday (0=Monday, 6=Sunday)
+
+  
+  ## DATETIME OBJECTS
+  # Get today's date from the datetime class
+
+  
+  # Get the current time
+
+ 
+
+  
+if __name__ == "__main__":
+  main();
+  
---
+++ module.py
@@ -0,0 +1,23 @@
+#
+# Exemple of code in here
+#
+
+# import math module to use it in math calculation
+import math
+
+# print the value of the sqrt
+print("The sequare of 16 is :", math.sqrt(16))
+
+# def main():
+
+def inc(a,b=1):
+    return(a+b)
+a=inc(1)
+a=inc(a,a)
+print(a)
+    
+
+   
+
+# if __name__ == "__main__":
+#     main()
