3b85a88fba0f92d1a9ae705ffe61f107d112c37c
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-03-05 04:10:37 +0000

    add loops file

---
+++ loops.py
@@ -0,0 +1,36 @@
+#
+# Exemple of code in here
+#
+
+
+
+def main():
+    x, y = 0, 10
+    # Define while loop
+    while(x < y):
+        print(x)
+        x = x + 1
+    # Define for loop
+
+    for x in range(5,10):
+        print(x)
+
+    # use for loop over a collection
+    days = ["Sat", "Sun", "Mon", "thu","Wen","Tur","Fri"]
+    for t in days:
+        print(t)
+    # use the break and the Continue in the loop
+    for x in range(0,10):
+        if(x==8): break
+        if(x % 2 == 0): continue
+        
+        print(x)
+    
+    # using the enumerate() funcion
+    days = ["Sat", "Sun", "Mon", "thu","Wen","Tur","Fri"]
+    for i,t in enumerate(days) :
+        print(i, t)
+   
+
+if __name__ == "__main__":
+    main()
