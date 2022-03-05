aed05ae8011cb26b305dfaddd44d1f64469b7c08
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-03-05 12:00:11 +0000

    add the class file

---
+++ class.py
@@ -0,0 +1,31 @@
+#
+# Exemple of code in here
+#
+
+class myClass():
+    def method1(self):
+        print("the first method1")
+
+    def method2(self, myString):
+        print("the first method2" + myString)
+
+class anotherClass(myClass):
+    def method1(self):
+        myClass.method1(self)
+        print("another class method1")
+
+    def method2(self, myString):
+        print("another class method2" + myString)
+
+def main():
+    c = myClass()
+    c.method1()
+    c.method2("nacer kraa")
+
+    c2 = anotherClass()
+    c2.method1()
+    c2.method2("this is another class")
+   
+
+if __name__ == "__main__":
+    main()
