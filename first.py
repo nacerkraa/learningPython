8077824d8267da03f18e6ed7352d79027807721d
nacer kraa <79715038+nacerkraa@users.noreply.github.com>
2022-04-11 03:14:38 +0100

    Update
    Add factorial function to the solution

--- first.py
+++ first.py
@@ -1,6 +1,10 @@
 def main():
     print("On the fonction2")
     print("my sub function1")
+def fact(num):
+    for(i in range(1,num) ):
+          num = num * i
 
 if __name__ == "__main__":
     main()
+    fact(25)
