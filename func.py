8418ee848c08b71ec5c67fd5f5c6ceb017c459d8
nacer kraa <79715038+nacerkraa@users.noreply.github.com>
2022-05-03 01:29:58 +0100

    Up

--- func.py
+++ func.py
@@ -2,13 +2,11 @@ def fun():
     print("On the function")
     return "hello world!" 
 
-def _cumpute_years(self):
-        for rec in self:
+def _cumpute_years(value):
+        
             today = date.today()
-            if rec.establish_date:
-                rec.age = today.year - rec.establish_date.year
-            else:
-                rec.age = 0
+            age = today.year - rec.establish_date.year
+            
 fun()
 print(fun())
 print(fun)
