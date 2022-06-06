f85b9e1263135df312ad16cf45b1c48641eefe62
nacer kraa <79715038+nacerkraa@users.noreply.github.com>
2022-06-06 19:13:25 +0000

    print element on array

--- array.py
+++ array.py
@@ -1,4 +1,7 @@
 # array on python
 array = ["1st", "2nd", "3rd"]
 # print the arrray
-print(array);
+print(array); # print 1st 2nd 3rd
+
+# print an element on array
+print array[0] # return string : '1st'
