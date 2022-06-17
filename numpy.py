77caed1cf00b80b377fc58df0aa4f1112ef42e63
nacer kraa <79715038+nacerkraa@users.noreply.github.com>
2022-06-17 09:16:10 +0000

    add more method

--- numpy.py
+++ numpy.py
@@ -1,3 +1,8 @@
 added = a+b #elementwise
 added_in_the_end = np.concatenate(a,b) #add at the end of the array
 #if you want to add in multiple dimentions check the documentation of np.concatenate
+
+# On the cmd
+
+>>> np.append([1, 2, 3], [[4, 5, 6], [7, 8, 9]])
+array([1, 2, 3, ..., 7, 8, 9])
