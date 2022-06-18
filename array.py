695ab424b7537154f876a7fb1d4112cfb414a031
nacer kraa <79715038+nacerkraa@users.noreply.github.com>
2022-06-18 09:22:09 +0000

    Update array.py

--- array.py
+++ array.py
@@ -1,7 +1,7 @@
 # array on python V1
 array = ["1.0st", "2.0nd", "3.0rd"]
 # print the arrray
-print(array); # print 1st 2nd 3rd
+print(array); # print 1.0st 2.0nd 3.0rd
 
 # print an element on array
 print array[0] # return string : '1st'
