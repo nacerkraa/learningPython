2494aaa0e77194be9531ca00b1d54077fafe0a8b
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-06-30 21:07:25 +0100

    roundom of float numbers

--- learnWithMash/script.py
+++ learnWithMash/script.py
@@ -1,10 +1,5 @@
 
-print(10/3)  # devition
-print(10//3) # model of the number
-print(10 ** 3) # Power
-
-x = 10
-
-x = x + 3
-
-x += 3 # agmented assigment operater (x*=3 or x-= 3)
+x = 1.5
+print(x)
+x = round(x)
+print(x)
