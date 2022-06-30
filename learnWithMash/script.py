c4f81a3440c2ee42b27113b09f97b0c0aee6a6b2
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-06-30 23:15:14 +0100

    while loop

--- learnWithMash/script.py
+++ learnWithMash/script.py
@@ -1,11 +1,5 @@
 
-is_hot = True
-is_sunny = True
-is_cold = False
-
-if(is_hot or is_sunny): # (and | or)
-    print("is hot")
-elif(is_cold):
-    print("is cold")
-else:
-    print("It's a levely day")
+i = 1
+while(i <= 5):
+    print(i)
+    i += 1
