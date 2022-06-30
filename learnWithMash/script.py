5ee242e2f2e5fba4ce21d4e85b9b76cb80cfac86
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-06-30 21:45:19 +0100

    update

--- learnWithMash/script.py
+++ learnWithMash/script.py
@@ -3,9 +3,9 @@ import math
 
 
 x = 1.5
-print("The Original Value is: " + x)
+print("The Original Value is: " + str(x))
 
-print("The Ciel of the Value is: " + math.ceil(x))
+print("The Ciel of the Value is: " + str(math.ceil(x)))
 
-print("The Floor of the Value is: " + math.floor(x))
+print("The Floor of the Value is: " + str(math.floor(x)))
 
