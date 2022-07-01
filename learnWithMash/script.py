49d744a77eb46200fc893243aa1a09c3409bd95b
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-01 23:52:44 +0100

    check if an item is exist

--- learnWithMash/script.py
+++ learnWithMash/script.py
@@ -3,19 +3,4 @@ import numbers
 
 numbers = [1, 3, 5, 6]
 
-numbers.append(15)  # for adding in the last of list
-print(numbers)
-
-numbers.insert(0, 1021) # for adding on the to top of the list
-print(numbers)
-
-numbers.remove(5) # remove an item from the list
-print(numbers)
-
-numbers.pop() # remove the last item from the list
-print(numbers)
-
-numbers.clear() # remove all the items
-
-
-print(numbers)
+print(30 in numbers)
