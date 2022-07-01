9303dd38bb645333b5485e803716649affa82415
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-01 17:44:58 +0100

    print the largest max number of a list

--- learnWithMash/script.py
+++ learnWithMash/script.py
@@ -1,3 +1,7 @@
-names = ["Levi","Falco","Gaby","Jhon","Mariem"]
+names = [5,8,7,9,2]
+max = 0
 
-print(names[2:])
+for item in names:
+    if item > max:
+        max = item
+print(f"The max of the list is: {max}")
