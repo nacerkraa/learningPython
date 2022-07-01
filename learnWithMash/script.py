61fe8d060183dc41522bc3926e2051bd30d9eef4
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-01 12:57:23 +0100

    Add more features to the programm

--- learnWithMash/script.py
+++ learnWithMash/script.py
@@ -1,16 +1,23 @@
 command = ""
+is_started = False
 
 while(command != "exit"):
     command = input("> ").lower()
-    if command == "start":
+    if command == "start" and is_started == False:
+        is_started = True
         print("the car is started...")
-    elif command == "stoped":
+    elif command == "start" and is_started == True:
+        print("The car is started already!!")
+    elif command == "stop" and is_started == True:
         print("the car is stopped...")
+        is_started = False
+    elif command == "stop" and is_started == False:
+        print("The car is already Stopped")
     elif command == "help":
         print('''
-            start- the car is started
-            stop- the car is stop the car
-            exit- for quit the car
+start- the car is started
+stop- the car is stop the car
+exit- for quit the car
         ''')
     elif command == "exit":
         print("quit the cmd")
