ddbec6ab225f09d20d951c571f336b4d05b03bf5
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-02 15:11:49 +0100

    Define an dictionary of emojis

--- learnWithMash/script.py
+++ learnWithMash/script.py
@@ -2,16 +2,6 @@
 
 
 dic = {
-    "1": "one",
-    "2": "two",
-    "3": "three",
-    "4": "four",
+    ":)": "🙂",
+    ":(": "🙁",
 }
-
-number = input("number: ")
-
-output = ""
-for ch in number:
-    output += dic.get(ch,"?") + " "
-
-print(output)
