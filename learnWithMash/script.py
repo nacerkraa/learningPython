0ce82e3c3d03bced6f0312bfeb2bd431e006ca26
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-02 09:39:04 +0100

    define a tuple

--- learnWithMash/script.py
+++ learnWithMash/script.py
@@ -1,14 +1,3 @@
+# Define a tuple
 
-from os import remove
-
-
-
-numberes = [6, 3, 1, 6, 5, 3]
-uniques = []
-
-
-for item in numberes:
-    if item not in uniques:
-        uniques.append(item)
-
-print(uniques)
+numberes = (6, 3, 1, 6, 5, 3)
