717b95568032f6fa2174b089621b3d6a8b4df99a
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-03 14:58:32 +0100

    Update

--- application/app.py
+++ application/app.py
@@ -1,9 +1,9 @@
 from finder import second_max_number,find_max
 from converters import lbs_to_kg
 NumberList = [5, 2, 3, 6]
-firstMax = find_max(NumberList)
-secondMax = second_max_number(NumberList)
 
-print(f"The first max is: {firstMax}")
-print(f"The second max is: {secondMax}")
+find_max(NumberList)
+second_max_number(NumberList)
+
+
 #print(lbs_to_kg(25))
