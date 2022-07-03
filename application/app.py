cff41a00031195460286e6840631042920911ea7
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-03 23:03:39 +0100

    Update max method

--- application/app.py
+++ application/app.py
@@ -1,9 +1,7 @@
-from finder import second_max_number,find_max
-from converters import lbs_to_kg
+
 NumberList = [5, 2, 3, 6]
 
-find_max(NumberList)
-second_max_number(NumberList)
+print(max(NumberList))
 
 
 #print(lbs_to_kg(25))
