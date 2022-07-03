5bb0b08c52d5aa6b5853541d98794b52db39c3e3
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-03 13:05:33 +0100

    Inheritance

--- learnWithMash/script.py
+++ learnWithMash/script.py
@@ -1,15 +1,12 @@
 # Define a function
 
-class Person:
-    def __init__(self, name):
-        self.name = name
+class Animal:
+    def walk():
+        print("Walking")
 
+class Dog(Animal):
+    pass
 
-    def Talk(self):
-        print(f"Hello, I'm {self.name}")
-
-person1 = Person("Ahmed")
-
-print(person1.name)
-
-person1.Talk()
+class Cat(Animal):
+    pass
+    
