af2f82685dc2a491b2cb05f41f01fbe7a9f337d3
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-03 11:03:14 +0100

    Add Zero Division Error

--- learnWithMash/script.py
+++ learnWithMash/script.py
@@ -1,1 +1,10 @@
+# Define a function
 
+try:
+    age = int(input("Age: "))
+    print(age)
+    age2 = age / 0
+except ZeroDivisionError:
+    print("Age can not divise by Zero!")
+except ValueError:
+    print("Invalid!")
