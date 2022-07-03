2d61ff0cba31c8bb2c17976f128852d9d94adb9f
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-03 14:36:26 +0100

    call two method

--- application/app.py
+++ application/app.py
@@ -1,5 +1,9 @@
-import finder
+from finder import second_max_number,find_max
 from converters import lbs_to_kg
 NumberList = [5, 2, 3, 6]
-print(finder.find_max(NumberList))
-print(lbs_to_kg(25))
+firstMax = find_max(NumberList)
+secondMax = second_max_number(NumberList)
+
+print(f"The first max is: {firstMax}")
+print(f"The second max is: {secondMax}")
+#print(lbs_to_kg(25))
--- application/finder.py
+++ application/finder.py
@@ -7,3 +7,7 @@ def find_max(NumberList):
         if number > max:
             max = number
     print(max)
+
+def second_max_number(NumberList):
+    NumberList.sort()
+    print(NumberList[-2])
